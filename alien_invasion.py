66a30dbe7e6bcbada903f21c6e7caa6c26830d8d
Kevin Menpara <202101079@daiict.ac.in>
2023-12-12 03:44:45 +0530

    Added ship.bmp at the bottom of screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # Initialize game and create a screen object.
@@ -8,6 +9,8 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    # Make a ship.
+    ship = Ship(screen)
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
@@ -16,6 +19,7 @@ def run_game():
                 sys.exit()
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
     
         # Make the most recently drawn screen visible.
         pygame.display.flip()
