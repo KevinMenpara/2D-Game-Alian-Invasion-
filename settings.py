b50ca6811eebd46cd2d47f1f3e3bbb4ce24a945a
Kevin Menpara <202101079@daiict.ac.in>
2023-12-12 02:57:51 +0530

    Added setting module for functionalities

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (204, 229, 255)
