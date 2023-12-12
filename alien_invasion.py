e665a12a899797170cddd79458a1d763da5c0248
Kevin Menpara <202101079@daiict.ac.in>
2023-12-13 02:07:32 +0530

    Bullets and shooting animation

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     # Initialize game and create a screen object.
@@ -12,10 +13,18 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # Make a ship.
     ship = Ship(ai_settings,screen)
+    # Make a group to store bullets in.
+    bullets = Group()
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
