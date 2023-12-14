febe93e94e9a30762487c82d11b534b6021e7622
Kevin Menpara <202101079@daiict.ac.in>
2023-12-15 01:54:54 +0530

    Completing functionalities of Game

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
 from alien import Alien
+from game_stats import GameStats
 
 def run_game():
     # Initialize game and create a screen object.
@@ -21,13 +22,16 @@ def run_game():
     alien = Alien(ai_settings, screen)
     # Create the fleet of aliens.
     gf.create_fleet(ai_settings, screen, ship, aliens)
+    # Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.update_bullets(bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -8,9 +8,15 @@ class Settings():
         self.bg_color = (204, 229, 255)
         # Ship settings
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
         # Bullet settings
         self.bullet_speed_factor = 0.79
         self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = 255, 00, 00
-        self.bullets_allowed = 3
+        self.bullets_allowed = 2
+        # Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
